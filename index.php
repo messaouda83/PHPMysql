309494107f7cac7f9fbe96b2224ea0fba39e2380
messaouda83 <messaoudabenchikh@yahoo.fr>
2021-04-26 11:50:22 +0200

    instruction echo en php

--- index.php
+++ index.php
@@ -5,14 +5,17 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Ceci est une page html de test</title>
+    <title>Notre première instruction : echo</title>
 </head>
 
 <body>
     <h2>Page de test</h2>
-    <p>
-        Cette page contient <strong>uniquement</strong> du code HTML.<br /> Voici quelques petits tests :
-    </p>
+    <h2>Affichage de texte avec PHP</h2>
+        
+        <p>
+            Cette ligne a été écrite entièrement en HTML.<br />
+            <?php echo "Celle-ci a été écrite entièrement en PHP."; ?>
+        </p>
 
     <ul>
         <li style="color:red;">Text en rouge</li>
