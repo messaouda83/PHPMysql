27408b00452e875e2c8e8427436f697024e49efb
messaouda83 <messaoudabenchikh@yahoo.fr>
2021-04-28 10:23:35 +0200

    Ajouter la variable message pour afficher le contenu du textarea

--- cible.php
+++ cible.php
@@ -1,5 +1,6 @@
 <p>Bonjour !</p>
 
 <p>Je sais comment tu t'appelles, hé hé. Tu t'appelles <?php echo $_POST['prenom']; ?> !</p>
+<p> Voici mon message: <?php  echo $_POST['message']?></p>
 
 <p>Si tu veux changer de prénom, <a href="index.php">clique ici</a> pour revenir à la page principale.</p>
